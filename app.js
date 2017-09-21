5fbff3dcda490461b84bd4958d0e802c12871905
Triste9 <oscar.99.tris@gmail.com>
2017-09-21 00:36:50 -0500

    make a good idea from my schol

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var session = require('express-session');
 const nodemailer = require('nodemailer');
 const Producto = require('./models/productos');
 const Proyecto = require('./models/proyectos');
+const aws = require('aws-sdk');
+const S3_BUCKET = process.env.S3_BUCKET;
+aws.config.region = 'us-west-2';
 
 app.use(express.static('public'));
 
@@ -329,5 +332,33 @@ app.get('/', (req, res, err) => {
 
     })
 });
+
+app.get('/account', (req, res) => res.render('ejm',{layout: false}));
+aws.config.region = 'eu-west-1';
+app.get('/sign-s3', (req, res) => {
+    const s3 = new aws.S3();
+    const fileName = req.query['file-name'];
+    const fileType = req.query['file-type'];
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Key: fileName,
+      Expires: 60,
+      ContentType: fileType,
+      ACL: 'public-read'
+    };
+  
+    s3.getSignedUrl('putObject', s3Params, (err, data) => {
+      if(err){
+        console.log(err);
+        return res.end();
+      }
+      const returnData = {
+        signedRequest: data,
+        url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+      };
+      res.write(JSON.stringify(returnData));
+      res.end();
+    });
+  });
 
 module.exports = app;
